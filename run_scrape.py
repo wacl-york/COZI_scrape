588979a9e4f7dc23949ef3f2e55c45f275bb024c
Stuart Lacy <stuart.lacy@york.ac.uk>
2021-03-08 14:59:53 +0000

    Reads data from local files
    The raw data is now saved locally from Google Drive using `rclone` in
    `sync_data_from_googledrive.sh`. This allows the data to be
    progressively synced rather than needing to download the full dataset
    each time using the GoogleDrive API as was being done previously.

--- run_scrape.py
+++ run_scrape.py
@@ -7,43 +7,23 @@
     suitable for plotting.
 """
 
-import shutil
 import argparse
 import os
-import io
 import json
 import pandas as pd
 import numpy as np
-from google.oauth2 import service_account
-import googleapiclient.discovery
-from googleapiclient.http import MediaIoBaseDownload
 from weatherlink.importer import Importer
 from weatherlink.models import convert_timestamp_to_datetime
 from weatherlink.utils import convert_fahrenheit_to_celsius, convert_miles_per_hour_to_meters_per_second
 
-LOCAL_DIR = "tmp"
+LOCAL_DIR = "raw_data"
 AQ_DIR = os.path.join(LOCAL_DIR, "AQ")
 MET_DIR = os.path.join(LOCAL_DIR, "MET")
-CREDENTIALS_FN = "credentials.json"
 COLUMNS_FN = "fields.json"
 
 
 def main():
     args = parse_args()
-
-    # Create local directory to store data
-    if os.path.exists(LOCAL_DIR):
-        cleanup()
-    os.makedirs(AQ_DIR)
-    os.makedirs(MET_DIR)
-
-    # Authenticate with Google Drive
-    try:
-        service = auth_google_api(CREDENTIALS_FN)
-    except GoogleAPIError:
-        print("Error connecting to service, terminating execution.")
-        cleanup()
-        return
 
     # Load field mapping dictionary
     try:
@@ -55,13 +35,11 @@ def main():
                 COLUMNS_FN
             )
         )
-        cleanup()
         return
     except json.decoder.JSONDecodeError:
         print(
             "Error: cannot parse {} as JSON, terminating execution.".format(COLUMNS_FN)
         )
-        cleanup()
         return
 
     try:
@@ -73,24 +51,21 @@ def main():
                 COLUMNS_FN
             )
         )
-        cleanup()
         return
 
     # Load both datasets into long data frames
     met_data = load_dataset(
-        service, "name contains '.wlk'", load_met_file, met_fields, MET_DIR
+        load_met_file, met_fields, MET_DIR
     )
     aq_data = load_dataset(
-        service, "name contains 'logging'", load_airquality_file, aq_fields, AQ_DIR
+        load_airquality_file, aq_fields, AQ_DIR
     )
 
     if met_data is None:
         print("Error: no clean meteorological data loaded, terminating execution.")
-        cleanup()
         return
     if aq_data is None:
         print("Error: no clean air quality data loaded, terminating execution.")
-        cleanup()
         return
 
     # Combine into a data frame, clean, and save to file
@@ -101,22 +76,6 @@ def main():
         print("Cleaned data saved to {}.".format(args.output))
     except FileNotFoundError:
         print("Cannot save to {}.".format(args.output))
-        cleanup()
-
-    cleanup()
-
-
-def cleanup():
-    """
-    Cleans up the working directory by removing temporary resources.
-
-    Args:
-        None.
-
-    Returns:
-        None, just deletes ancillary files.
-    """
-    shutil.rmtree(LOCAL_DIR)
 
 
 def parse_args():
@@ -156,63 +115,12 @@ def convert_excel_time(excel_time):
     return converted.dt.round("S")
 
 
-class GoogleAPIError(Exception):
+def load_dataset(load_function, fields, localdir):
     """
-    Custom exception class for errors related to using Google's API.
-    """
-
-
-def auth_google_api(filename):
-    """
-    Authorizes connection to GoogleDrive API.
-
-    Requires GOOGLE_CREDS environment variable to be set, containing the
-    contents of the JSON credential file, formatted as a string.
-
-    Uses v3 of the GoogleDrive API, see examples at:
-    https://developers.google.com/drive/api/v3/quickstart/python
+    Loads a dataset from disk into memory, combining all observations into a
+    single long pandas.DataFrame.
 
     Args:
-        - filename (str): Location of JSON containing credentials for service
-        account.
-
-    Returns:
-        A googleapiclient.discovery.Resource object.
-    """
-    scopes = ["https://www.googleapis.com/auth/drive.readonly"]
-
-    try:
-        credentials = service_account.Credentials.from_service_account_file(
-            filename, scopes=scopes
-        )
-    except FileNotFoundError:
-        raise GoogleAPIError(
-            "Credential file '{}' not found".format(filename)
-        ) from None
-    except ValueError:
-        raise GoogleAPIError("Credential file is not formatted as expected") from None
-
-    # setting cache_discovery = False removes a large amount of warnings in log,
-    # that seemingly have little performance impact as we don't need cache.
-    service = googleapiclient.discovery.build(
-        "drive", "v3", credentials=credentials, cache_discovery=False
-    )
-    return service
-
-
-def load_dataset(service, query, load_function, fields, tempdir):
-    """
-    Loads a dataset that is stored on Google Drive into local memory.
-
-    It queries the Google Drive API to find all the files from this dataset,
-    downloads them all to a local temporary location, reads them into Pandas
-    DataFrame objects, and converts them into a single long DataFrame.
-
-    Args:
-        - service (googleapiclient.discovery.Resource): A handle to a Google API
-            service account.
-        - query (string): The query string used to find the files to download
-            from Google Drive. See https://developers.google.com/drive/api/v3/search-files
         - load_function (function): The function to use to load a data file from
             this dataset into Pandas. It needs to be parameterised to accept 2
             arguments:
@@ -222,7 +130,7 @@ def load_dataset(service, query, load_function, fields, tempdir):
         - fields (dict): Mapping between {raw_label: clean_label}, where
             raw_label is the column name in the raw data on Google Drive, and
             clean_label is the desired label for our output data.
-        - tempdir (string): Filepath to a temporary local directory where files
+        - localdir (string): Filepath to a temporary local directory where files
             can be downloaded to.
 
     Returns:
@@ -231,21 +139,11 @@ def load_dataset(service, query, load_function, fields, tempdir):
             - measurand: Name of measurand as human readable string
             - value: Measurement value as float.
     """
-
-    # Obtain reference to all logging files
-    results = service.files().list(q=query, fields="files(id, name)").execute()
-    items = results.get("files", [])
-
-    # Download files
-    for item in items:
-        print("Downloading {}...".format(item["name"]))
-        download_file(item["id"], os.path.join(tempdir, item["name"]), service)
-
     # Load all data into a single data frame
-    print("Processing data into single file...")
+    print(f"Processing data from {localdir} into single file...")
     dfs = []
-    for fn in os.listdir(tempdir):
-        df = load_function(os.path.join(tempdir, fn), fields)
+    for fn in os.listdir(localdir):
+        df = load_function(os.path.join(localdir, fn), fields)
         if df is None:
             continue
 
@@ -259,31 +157,6 @@ def load_dataset(service, query, load_function, fields, tempdir):
         combined = None
 
     return combined
-
-
-def download_file(file_id, filename, service):
-    """
-    Downloads a file from Google Drive.
-
-    Args:
-        - file_id (str): File ID as used by Google Drive.
-        - filename (str): File path to save file to.
-        - service (Resource): A googleapiclient.discovery.Resource object.
-
-    Returns:
-        None, saves file to disk as a side-effect.
-    """
-    request = service.files().get_media(fileId=file_id)
-    fh = io.BytesIO()
-    downloader = MediaIoBaseDownload(fh, request)
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-        print("Download %d%%." % int(status.progress() * 100))
-
-    fh.seek(0)
-    with open(filename, "wb") as outfile:
-        shutil.copyfileobj(fh, outfile)
 
 
 def load_airquality_file(filename, fields):
