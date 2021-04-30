6652d73a45ebdf8f5b0528bd499d2e98199bf591
S Lacy <sl561@york.ac.uk>
2021-04-30 11:43:48 +0100

    Added production settings

--- sync_data_from_googledrive.sh
+++ sync_data_from_googledrive.sh
@@ -5,4 +5,4 @@ cd ~/WACL/COZI_scrape
 rclone --config rclone.conf --include *.wlk -v --drive-shared-with-me sync CoziDrive:WACLroof raw_data/MET
 rclone --config rclone.conf --include logging* -v --drive-shared-with-me sync CoziDrive:COZI_DATA raw_data/AQ
 
-# TODO Add in deployment call to the python environment to run the scraper and save file in the appropriate folder
+~/.conda/envs/coziscrape/bin/python run_scrape.py /shared/storage/shiny0/cozi/data.csv
