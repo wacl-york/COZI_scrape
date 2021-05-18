19854e3f104f25ce1271272ed5ba056d0a1aef87
Stuart Lacy <stuart.lacy@york.ac.uk>
2021-05-18 17:58:02 +0100

    Uploads COZI data to Google Drive for Grafana
    NCAS have a cron job to pull the CSV from Google Drive into a DB that
    feeds into Grafana.

--- sync_data_from_googledrive.sh
+++ sync_data_from_googledrive.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-# Runs the COZI scraper
 cd ~/WACL/COZI_scrape
+
+# Downloads latest raw data
 rclone --config rclone.conf --include *.wlk -v --drive-shared-with-me sync CoziDrive:WACLroof raw_data/MET
 rclone --config rclone.conf --include logging* -v --drive-shared-with-me sync CoziDrive:COZI_DATA raw_data/AQ
 
-~/.conda/envs/coziscrape/bin/python run_scrape.py /shared/storage/shiny0/cozi/data.csv
+# Pre-processes it into a single CSV
+/.conda/envs/coziscrape/bin/python run_scrape.py clean_data/cozi_data.csv
+
+# Uploads for Shiny
+cp clean_data/cozi_data.csv /shared/storage/shiny0/cozi/data.csv
+# Uploads for Grafana
+rclone --config rclone.conf --include cozi_data.csv -v --drive-shared-with-me sync clean_data CoziDrive:COZIDataForGrafana
